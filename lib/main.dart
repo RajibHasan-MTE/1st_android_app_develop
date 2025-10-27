d567865a1b8b3a1158a5252ae9daf03d91ac6335
Rajib Hasan <rajibhasan.mte@mail.com>
2025-10-28 01:37:30 +0600

    added last humanoid robot pic on my wallpaper

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,235 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); // Application entry point
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme: ThemeData(primarySwatch: Colors.amber),
+      //color: Color.blue,
+      debugShowCheckedModeBanner: false,
+      home: HomeActivity(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  MySnackBar(message, context){
+    return ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(message))
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.black12,
+        title: Text("ROBOTICS WITH RAJIB", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+        titleSpacing: 0,
+        //centerTitle: true,
+        toolbarHeight: 60,
+        toolbarOpacity: 1,
+        elevation: 0,
+        actions: [
+          IconButton(onPressed: (){MySnackBar("I am Comments", context);}, icon: Icon(Icons.comment)),
+          IconButton(onPressed: (){MySnackBar("I am Search", context);}, icon: Icon(Icons.search)),
+          IconButton(onPressed: (){MySnackBar("I am Setting", context);}, icon: Icon(Icons.settings)),
+          IconButton(onPressed: (){MySnackBar("I am email", context);}, icon: Icon(Icons.email)),
+          IconButton(onPressed: (){MySnackBar("Facebook profile", context);}, icon: Icon(Icons.facebook)),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+
+      /***************************************** Floating Action Button **************************************
+       * Floating Action button is button that is placed at the right bottom corner by default.
+       * Floating Action Button is an Icon button tha floats over ther context of the screen at a fixed placed.
+       ********************************************************************************************************/
+      floatingActionButton: FloatingActionButton(
+        elevation: 0,
+        backgroundColor: Colors.green,
+        onPressed: (){
+          MySnackBar("I am floating action button", context);
+        },
+        child: Icon(Icons.add),
+      ),
+
+      /**************************************** Bottom Navigation Bar *******************************************
+       * Bottom Navigation Bar is a bar that is placed at the bottom of the screen.
+       * Bottom Navigation Bar is used to navigate between different screens.
+       * Manu at the bottom of the Scaffold.
+       * We have seen this Navigation bar in most of the application.
+       * 1st call bottomNavigationBar properties then call BottomNavigationBarItem class
+       * then object: items
+       * onTap: property is used to navigate between different screens.
+       **********************************************************************************************************/
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1,
+        //elevation: 0,
+        //backgroundColor: Colors.amber,
+        //selectedItemColor: Colors.green,
+        //unselectedItemColor: Colors.black,
+        //showUnselectedLabels: true,
+        //showSelectedLabels: true,
+        //type: BottomNavigationBarType.fixed,
+        //onTap: (index){
+        //MySnackBar("I am index $index", context);
+        // },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: "Contact"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+        onTap: (int index){
+          if(index == 0){
+            MySnackBar("I am home bottom manu", context);
+          }
+          if(index == 1){
+            MySnackBar("I am contact bottom manu", context);
+          }
+          if(index == 2){
+            MySnackBar("I am profile botton manu", context);
+          }
+        },
+      ),
+
+      /********************************** Navigation drawer *************************************************
+       * Navigation drawer is a drawer that is placed on the left side of the screen.
+       * Navigation drawer is used to navigate between different screens.
+       * Drawer is used to navigate between different screens.
+       * drawer has two parts: header and body.
+       * header is used to display the logo and the name of the app.
+       * body is used to display the menu items.
+       * title: property is used to display the title of the menu item.
+       * leading: property is used to display the icon of the menu item.
+       * trailing: property is used to display the icon of the menu item.
+       * onTap: property is used to navigate between different screens.
+       *
+       ******************************************************************************************************/
+      //endDrawer: Drawer(
+      endDrawer: Drawer(
+        child: ListView(
+          children: [
+            /*
+            DrawerHeader(
+              padding: EdgeInsets.all(0),
+              child: UserAccountsDrawerHeader(
+                decoration: BoxDecoration(color: Colors.black),
+                  accountName: Text("Rajib Hasasn", style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                  accountEmail: Text("rajibhasan.mte@gmail.com"),
+                  currentAccountPicture: Image.network("https://avatars.githubusercontent.com/u/207505410?s=400&u=2ca2167fb01cf553acca36a0ee9ce7131deea653&v=4", width: 200, height: 200)
+              ),
             ),
+             */
+            UserAccountsDrawerHeader(
+              accountName: const Text("Rajib Hasan", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              accountEmail: const Text("Studies BSc in Mechatronics engineering", style: TextStyle(fontSize: 13, fontWeight: FontWeight.normal)),
+              currentAccountPicture: const CircleAvatar(
+                backgroundImage: NetworkImage(
+                  "https://avatars.githubusercontent.com/u/207505410?s=400&u=2ca2167fb01cf553acca36a0ee9ce7131deea653&v=4",
+                ),
+              ),
+            ),
+
+            ListTile(
+                title: Text("Home"),
+                leading: Icon(Icons.home),
+                //trailing: Icon(Icons.arrow_forward),
+                onTap: (){
+                  MySnackBar("Home", context);
+                }
+            ),
+            ListTile(
+                title: Text("Email"),
+                leading: Icon(Icons.email),
+                //trailing: Icon(Icons.arrow_forward),
+                onTap: (){
+                  MySnackBar("Email", context);
+                }
+            ),
+            ListTile(
+                title: Text("Phone"),
+                leading: Icon(Icons.phone),
+                //trailing: Icon(Icons.arrow_forward),
+                onTap: (){
+                  MySnackBar("Phone", context);
+                }
+            ),
+
           ],
         ),
+
+
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            /*
+            DrawerHeader(
+              padding: EdgeInsets.all(0),
+              child: UserAccountsDrawerHeader(
+                decoration: BoxDecoration(color: Colors.black),
+                  accountName: Text("Rajib Hasasn", style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                  accountEmail: Text("rajibhasan.mte@gmail.com"),
+                  currentAccountPicture: Image.network("https://avatars.githubusercontent.com/u/207505410?s=400&u=2ca2167fb01cf553acca36a0ee9ce7131deea653&v=4", width: 200, height: 200)
+              ),
+            ),
+             */
+            UserAccountsDrawerHeader(
+              accountName: const Text("Rajib Hasan", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              accountEmail: const Text("Studies BSc in Mechatronics engineering", style: TextStyle(fontSize: 13, fontWeight: FontWeight.normal)),
+              currentAccountPicture: const CircleAvatar(
+                backgroundImage: NetworkImage(
+                  "https://avatars.githubusercontent.com/u/207505410?s=400&u=2ca2167fb01cf553acca36a0ee9ce7131deea653&v=4",
+                ),
+              ),
+            ),
+
+            ListTile(
+                title: Text("Home"),
+                leading: Icon(Icons.home),
+                //trailing: Icon(Icons.arrow_forward),
+                onTap: (){
+                  MySnackBar("Home", context);
+                }
+            ),
+            ListTile(
+                title: Text("Email"),
+                leading: Icon(Icons.email),
+                //trailing: Icon(Icons.arrow_forward),
+                onTap: (){
+                  MySnackBar("Email", context);
+                }
+            ),
+            ListTile(
+                title: Text("Phone"),
+                leading: Icon(Icons.phone),
+                //trailing: Icon(Icons.arrow_forward),
+                onTap: (){
+                  MySnackBar("Phone", context);
+                }
+            ),
+
+          ],
+        ),
+
+
+      ),
+      body: Center(
+        //child: Text("Welcome in Inventory App"),
+        //child: Image.network("https://pal-robotics.com/wp-content/uploads/2025/01/TIAGoPro_Sided1_Mobile-Manipulator_PAL-Robotics-921x1024.webp"),
+        child: Image.network("https://pal-robotics.com/wp-content/uploads/2025/01/Kangaroo-Pro-frontal-image-PAL-Robotics-legged-humanoid-scaled-e1760631086520-703x1024.png"),
+      ),
+
     );
   }
 }
