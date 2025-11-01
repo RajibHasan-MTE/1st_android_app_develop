dc3c8b7b60a46bca01fc1ec1e0de88130ede68dc
Rajib Hasan <rajibhasan.mte@mail.com>
2025-11-01 19:53:27 +0600

    basic code create

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
-import 'package:app1/Fragment/HomeFragment.dart';
 import 'package:flutter/material.dart';
 
-import 'Fragment/AlarmFragment.dart';
-import 'Fragment/BalanceFragment.dart';
-import 'Fragment/ContactFragment.dart';
-import 'Fragment/EmailFragment.dart';
-import 'Fragment/PersonFragment.dart';
-import 'Fragment/SearchFragment.dart';
-import 'Fragment/SettingsFragment.dart';
+
 
 void main() {
   runApp(const MyApp()); // Application entry point
@@ -42,67 +35,13 @@ class HomeActivity extends StatelessWidget {
         appBar: AppBar(
           title: Text('Home'),
         ),
-        body: Center(
-          child: Icon(
-            Icons.camera_alt,
-            size: 100,
-            color: Colors.black12,
-          ),
+        body: Container(
+            width: double.infinity,
+            height: 300,
+            color: Colors.red,
+            alignment: Alignment.center,
         )
     );
   }
 }
 
-
-class Activity1 extends StatelessWidget {
-  String msg;
-
-  Activity1(
-      this.msg,
-      {super.key}
-  );
-
-
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(msg),
-      ),
-      body: Center(
-        child: ElevatedButton(onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => Activity2("This is from home to activity-2")));
-        }, child: Text("Go Activity-2"),),
-      ),
-    );
-  }
-}
-
-
-class Activity2 extends StatelessWidget {
-  String msg;
-
-  Activity2(
-      this.msg,
-      {super.key}
-      );
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(msg),
-      ),
-      body: Center(
-        child: ElevatedButton(onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => Activity1("This is from home to activity-1")));
-        }, child: Text("Go Activity-1"),),
-      ),
-    );
-  }
-}
