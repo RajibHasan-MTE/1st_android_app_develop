813a519a477a74f299ea6819ede77f8d7786bfff
Rajib Hasan <rajibhasan.mte@mail.com>
2025-11-05 23:42:26 +0600

    successfully work boostrap container

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:app1/style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bootstrap/flutter_bootstrap.dart';
 import 'package:responsive_grid/responsive_grid.dart';
 
 
@@ -39,46 +40,54 @@ class HomeActivity extends StatelessWidget {
         ),
         body: SingleChildScrollView(
           scrollDirection: Axis.vertical,
-          child: ResponsiveGridRow(
+          child: BootstrapContainer(
+            fluid: true,
+            //fluid: false,
             children: [
-              ResponsiveGridCol(
-                lg: 12,
-                child: Container(
-                  height: 100,
-                  color: Colors.orange,
-                ),
-              ),
-              ResponsiveGridCol(
-                xl: 3, lg: 3, md: 3, sm: 12, xs: 12,
-                child: Container(
-                  height: 100,
-                  color: Colors.green,
-                ),
-              ),
-              ResponsiveGridCol(
-                xl: 3, lg: 3, md: 3, sm: 12, xs: 12,
-                child: Container(
-                  height: 100,
-                  color: Colors.red,
-                ),
-              ),
-              ResponsiveGridCol(
-                xl: 3, lg: 3, md: 3, sm: 12, xs: 12,
-                child: Container(
-                  height: 100,
-                  color: Colors.black,
-                ),
-              ),
-              ResponsiveGridCol(
-                xl: 3, lg: 3, md: 3, sm: 12, xs: 12,
-                child: Container(
-                  height: 100,
-                  color: Colors.yellow,
-                ),
-              ),
-
+              BootstrapRow(
+                height: 100,
+                children: [
+                  BootstrapCol(
+                    sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                    child: Container(height: 100, color: Colors.red,),
+                  ),
+                  BootstrapCol(
+                      sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                      child: Container(height: 100, color: Colors.black,)
+                  ),
+                  BootstrapCol(
+                      sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                      child: Container(height: 100, color: Colors.yellow,)
+                  ),
+                  BootstrapCol(
+                    sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                    child: Container(height: 100, color: Colors.red,),
+                  ),
+                  BootstrapCol(
+                      sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                      child: Container(height: 100, color: Colors.black,)
+                  ),
+                  BootstrapCol(
+                      sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                      child: Container(height: 100, color: Colors.yellow,)
+                  ),
+                  BootstrapCol(
+                    sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                    child: Container(height: 100, color: Colors.red,),
+                  ),
+                  BootstrapCol(
+                      sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                      child: Container(height: 100, color: Colors.black,)
+                  ),
+                  BootstrapCol(
+                      sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                      child: Container(height: 100, color: Colors.yellow,)
+                  ),
+                ],
+              )
             ],
           ),
+
         )
     );
   }
