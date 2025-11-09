c23d67a9ba4715b9709c50545df87e603ad6d14b
Rajib Hasan <rajibhasan.mte@mail.com>
2025-11-09 11:58:44 +0600

    successfully: stateful conter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,55 @@
-import 'package:app1/style.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bootstrap/flutter_bootstrap.dart';
-import 'package:responsive_grid/responsive_grid.dart';
-
 
 void main() {
-  runApp(const MyApp()); // Application entry point
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.green),
-      darkTheme: ThemeData(primarySwatch: Colors.amber),
-      //color: Color.blue,
-      debugShowCheckedModeBanner: false,
-      home: HomeActivity(),
+      title: 'Counter app',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: MyHomePage(),
     );
   }
 }
 
-
-
-
-class HomeActivity extends StatelessWidget {
-  //const HomeActivity({super.key});
-
+class MyHomePage extends StatefulWidget {
 
   @override
-  Widget build(BuildContext context) {
-
-
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Rajib Hasan'),
-        ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: BootstrapContainer(
-            fluid: true,
-            //fluid: false,
-            children: [
-              BootstrapRow(
-                height: 100,
-                children: [
-                  BootstrapCol(
-                    sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
-                    child: Container(height: 100, color: Colors.red,),
-                  ),
-                  BootstrapCol(
-                      sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
-                      child: Container(height: 100, color: Colors.black,)
-                  ),
-                  BootstrapCol(
-                      sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
-                      child: Container(height: 100, color: Colors.yellow,)
-                  ),
-                  BootstrapCol(
-                    sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
-                    child: Container(height: 100, color: Colors.red,),
-                  ),
-                  BootstrapCol(
-                      sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
-                      child: Container(height: 100, color: Colors.black,)
-                  ),
-                  BootstrapCol(
-                      sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
-                      child: Container(height: 100, color: Colors.yellow,)
-                  ),
-                  BootstrapCol(
-                    sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
-                    child: Container(height: 100, color: Colors.red,),
-                  ),
-                  BootstrapCol(
-                      sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
-                      child: Container(height: 100, color: Colors.black,)
-                  ),
-                  BootstrapCol(
-                      sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
-                      child: Container(height: 100, color: Colors.yellow,)
-                  ),
-                ],
-              )
-            ],
-          ),
-
-        )
-    );
+  State<StatefulWidget> createState(){
+    //print("CreateState called");
+    return MyHomePageUI();
   }
+
 }
 
+
+class MyHomePageUI extends State<MyHomePage>{
+  int countNumber = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Counter App"),),
+      body: Center(
+        child: Text(countNumber.toString()),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: (){
+            setState(() {
+              countNumber += 1;
+            });
+            print(countNumber);
+          }
+      ),
+    );
+  }
+
+}
