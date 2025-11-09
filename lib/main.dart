77ebe36ec54cfcb80e19c7f5ac61e3d848d8f8c3
Rajib Hasan <rajibhasan.mte@mail.com>
2025-11-09 14:32:56 +0600

    page successfuly run

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,28 @@
+import 'package:app1/ToDoPage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'Style.dart';
 
-void main() {
-  runApp(const MyApp());
+
+
+
+
+
+void main(){
+
+  runApp(MyApp());
+
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget{
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sum app',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-
-  @override
-  State<StatefulWidget> createState(){
-    return MyHomePageUI();
-  }
-
-}
-
-
-class MyHomePageUI extends State<MyHomePage>{
-  Map<String, double> FormValue = { "Num1" : 0, "Num2" : 0, "Num3" : 0};
-  double SUM = 0;
-  int countNumber = 0;
-  @override
-  Widget build(BuildContext context) {
-
-    MyInputOnChange(InputKey, InputValue){
-      setState(() {
-        FormValue.update(InputKey, (value) => double.parse(InputValue));
-      });
-
-    }
-
-    AddAllNumber(){
-      setState(() {
-        SUM = FormValue['Num1']! + FormValue['Num2']! + FormValue['Num3']!;
-      });
-
-    }
-
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sum app'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(SUM.toString(), style: HeadTexStyle(),),
-            SizedBox(height: 20,),
-            TextFormField(
-              onChanged: (value){
-                MyInputOnChange("Num1", value);
-              }, decoration: AppInputStyle("First number"),),
-
-            SizedBox(height: 20),
-            TextFormField(
-              onChanged: (value){
-                MyInputOnChange("Num2", value);
-              }, decoration: AppInputStyle("Second number"),),
-
-            SizedBox(height: 20),
-            TextFormField(onChanged: (value){
-              MyInputOnChange("Num3", value);
-            }, decoration: AppInputStyle("Third number"),),
-
-            SizedBox(height: 20),
-            Container(
-              width: double.infinity,
-              height: 50,
-              child: ElevatedButton(
-              style: AppButtonStyle(),
-              child: Text("Add"),
-              onPressed: (){
-                  AddAllNumber();
-              }
-              ),
-            ),
-          ],
-        ),
-      )
+      title: "To Do",
+      theme: ThemeData(primaryColor: Colors.green),
+      home: ToDoPage(),
     );
   }
 
