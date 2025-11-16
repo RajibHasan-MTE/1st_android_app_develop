69b935b04684d7602172d7b482ac4c0e9ee6a8ce
Rajib Hasan <rajibhasan.mte@mail.com>
2025-11-17 01:46:50 +0600

    succesfully run program

--- lib/ToDoPage.dart
+++ lib/ToDoPage.dart
@@ -12,10 +12,42 @@ class ToDoPage extends StatefulWidget{
 }
 
 class ToDoPageView extends State<ToDoPage>{
+
+  List ToDoList = [{"1" : "1"}, {"2" : "2"}, {"3" : "3"}];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("To Do"),),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 10,
+              child: Row(
+                children: [
+                  Expanded(child: TextFormField()),
+                  Expanded(child: ElevatedButton(onPressed: (){}, child: Text('Add'))),
+                ],
+              )
+            ),
+            Expanded(
+              flex: 90,
+              child: ListView.builder(
+                    itemCount: ToDoList.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Text("List"),
+                      );
+                    }
+              ),
+              //child: Text("List View"),
+
+            ),
+          ],
+        ),
+      )
     );
   }
 
