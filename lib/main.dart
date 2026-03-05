c7a9fdc6dfb161c1c7abf727a75e99038be1a4b8
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-03-06 03:21:20 +0600

    completed class

--- lib/main.dart
+++ lib/main.dart
@@ -40,100 +40,208 @@ class HomePage extends StatelessWidget {
     //     )
     //   ],
     // )
+    //   body: Row(
+    //     children: [
+    //       Expanded(
+    //         flex: 8,
+    //         child: Container(
+    //           width: 200,
+    //           // height: MediaQuery.sizeOf(context).height/2,
+    //           color: Colors.red,
+    //
+    //         ),
+    //       ),
+    //       Expanded(
+    //         flex: 2,
+    //         child: Container(
+    //           width: 200,
+    //           // height: MediaQuery.sizeOf(context).height/2,
+    //           color: Colors.blue,
+    //
+    //         ),
+    //       ),
+    //       Expanded(
+    //         child: Container(
+    //           width: 200,
+    //           color: Colors.yellow,
+    //         ),
+    //       ),
+    //       // Container(
+    //       //   width: 100,
+    //       //   height: 100,
+    //       //   color: Colors.blue,
+    //       //
+    //       // ),
+    //       // Container(
+    //       //   width: 100,
+    //       //   height: 100,
+    //       //   color: Colors.red,
+    //       //
+    //       // ),
+    //       // Container(
+    //       //   width: 100,
+    //       //   height: 100,
+    //       //   color: Colors.blue,
+    //       //
+    //       // ),
+    //       // Container(
+    //       //   width: 100,
+    //       //   height: 100,
+    //       //   color: Colors.red,
+    //       //
+    //       // ),
+    //       // Container(
+    //       //   width: 100,
+    //       //   height: 100,
+    //       //   color: Colors.blue,
+    //       //
+    //       // ),
+    //       // Container(
+    //       //   width: 100,
+    //       //   height: 100,
+    //       //   color: Colors.red,
+    //       //
+    //       // ),
+    //       // Container(
+    //       //   width: 100,
+    //       //   height: 100,
+    //       //   color: Colors.blue,
+    //       //
+    //       // ),
+    //       // Container(
+    //       //   width: 100,
+    //       //   height: 100,
+    //       //   color: Colors.red,
+    //       //
+    //       // ),
+    //       // Container(
+    //       //   width: 100,
+    //       //   height: 100,
+    //       //   color: Colors.blue,
+    //       //
+    //       // ),
+    //       // Container(
+    //       //   width: 100,
+    //       //   height: 100,
+    //       //   color: Colors.red,
+    //       //
+    //       // ),
+    //       // Container(
+    //       //   width: 100,
+    //       //   height: 100,
+    //       //   color: Colors.blue,
+    //       //
+    //       // ),
+    //     ],
+    //
+    //   ),
 
 /* ----------------------- End Row ------------------------------ */
 
 /* ----------------------- Start Row ------------------------------ */
-      body: Column(
-        children: [
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.red,
-
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.blue,
-
-          ),
+      // body: Row(
+      //   children: [
+        /* -------------- before expended ------------ */
           // Container(
           //   width: 100,
-          //   height: 100,
+          //   height: MediaQuery.sizeOf(context).height/2,
           //   color: Colors.red,
           //
           // ),
           // Container(
           //   width: 100,
-          //   height: 100,
+          //   height: MediaQuery.sizeOf(context).height/2,
           //   color: Colors.blue,
           //
           // ),
-          // Container(
-          //   width: 100,
-          //   height: 100,
-          //   color: Colors.red,
+        /* ----------- After Expended ---------------*/
+          // Expanded(
+          //   flex: 8,
+          //   child: Container(
+          //     width: 200,
+          //     // height: MediaQuery.sizeOf(context).height/2,
+          //     color: Colors.red,
           //
+          //   ),
           // ),
-          // Container(
-          //   width: 100,
-          //   height: 100,
-          //   color: Colors.blue,
+          // Expanded(
+          //   flex: 2,
+          //   child: Container(
+          //     width: 200,
+          //     // height: MediaQuery.sizeOf(context).height/2,
+          //     color: Colors.blue,
           //
+          //   ),
           // ),
-          // Container(
-          //   width: 100,
-          //   height: 100,
-          //   color: Colors.red,
-          //
+          // Expanded(
+          //   child: Container(
+          //     width: 200,
+          //     color: Colors.yellow,
+          //   ),
           // ),
-          // Container(
-          //   width: 100,
-          //   height: 100,
-          //   color: Colors.blue,
-          //
-          // ),
-          // Container(
-          //   width: 100,
-          //   height: 100,
-          //   color: Colors.red,
-          //
-          // ),
-          // Container(
-          //   width: 100,
-          //   height: 100,
-          //   color: Colors.blue,
-          //
-          // ),
-          // Container(
-          //   width: 100,
-          //   height: 100,
-          //   color: Colors.red,
-          //
-          // ),
-          // Container(
-          //   width: 100,
-          //   height: 100,
-          //   color: Colors.blue,
-          //
-          // ),
-          // Container(
-          //   width: 100,
-          //   height: 100,
-          //   color: Colors.red,
-          //
-          // ),
-          // Container(
-          //   width: 100,
-          //   height: 100,
-          //   color: Colors.blue,
-          //
-          // ),
-        ],
-
-        ),
+        /* ------------- Flexible --------------- */
+        //   Container(color: Colors.blue, width: 200, child: Text("Rajib Hasan"),),
+        //   Flexible(
+        //     // fit: FlexFit.tight,
+        //     fit: FlexFit.loose,
+        //     child: Text("ery very very very very very very very very very veryery very very very very very very very very very veryery very very very very very very very very very veryvery very very very very very very very very very very"),
+        //   ),
+        //
+        // ],
+        //
+        // ),
 /* ----------------------- End Column ------------------------------ */
+/* ----------------------- Start wrap ------------------------------ */
+    // body: Wrap(
+    //   spacing: 8, runSpacing: 8,
+    //   children: List.generate(10, (index) => Chip(label: Text("Item $index"),),),
+    //
+    // ),
+/* ----------------------- Start mergin and padding ------------------------------ */
+    // body: Padding(
+    //   padding: const EdgeInsets.all(8.0),
+    //   child: Container(
+    //     padding: EdgeInsets.all(10),
+    //     child: Column(
+    //       children: [
+    //         Container(
+    //           margin: EdgeInsets.all(8),
+    //           color: Colors.amber,
+    //           height: 200,
+    //         ),
+    //         Container(
+    //           // margin: EdgeInsets.all(8),
+    //           color: Colors.red,
+    //           height: 200,
+    //         ),
+    //         Container(
+    //           // margin: EdgeInsets.all(8),
+    //           color: Colors.black,
+    //           height: 200,
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // ),
+/* ----------------------- Start Align ------------------------------ */
+    // body: Align(
+    //   alignment: Alignment(-1, 1),
+    //   child: Icon(Icons.email_outlined),
+    // ),
+/* ----------------------- Start Align ------------------------------ */
+    // body: Center(
+    //   widthFactor: 2,
+    //   heightFactor: 3,
+    //   child: Text("Rajib Hasan"),
+    // ),
+/* ----------------------- Start Align ------------------------------ */
+    body: Container(
+      width: 100,
+      height: 50,
+      child: FittedBox(
+        fit: BoxFit.contain, // preferable
+        child: Text("Rajib Hasan sdfasdfsdf"),),
+    ),
     );
 
   }
