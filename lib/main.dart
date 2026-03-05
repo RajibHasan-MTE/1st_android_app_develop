44504dbaeb997d89c85c07ed987282e65c0ed067
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-03-05 15:18:29 +0600

    added: column structure code

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,119 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Rajib Hasan"),),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Rajib Hasan"),
+      ),
+/* ----------------------- Start Row ------------------------------ */
+    // body: Row(
+    //   children: [
+    //     Container(
+    //       width: 100,
+    //       height: 100,
+    //       color: Colors.red,
+    //     ),
+    //     Container(
+    //       width: 100,
+    //       height: 100,
+    //       color: Colors.blue,
+    //     )
+    //   ],
+    // )
 
+/* ----------------------- End Row ------------------------------ */
+
+/* ----------------------- Start Row ------------------------------ */
+      body: Column(
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.red,
+
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.blue,
+
+          ),
+          // Container(
+          //   width: 100,
+          //   height: 100,
+          //   color: Colors.red,
+          //
+          // ),
+          // Container(
+          //   width: 100,
+          //   height: 100,
+          //   color: Colors.blue,
+          //
+          // ),
+          // Container(
+          //   width: 100,
+          //   height: 100,
+          //   color: Colors.red,
+          //
+          // ),
+          // Container(
+          //   width: 100,
+          //   height: 100,
+          //   color: Colors.blue,
+          //
+          // ),
+          // Container(
+          //   width: 100,
+          //   height: 100,
+          //   color: Colors.red,
+          //
+          // ),
+          // Container(
+          //   width: 100,
+          //   height: 100,
+          //   color: Colors.blue,
+          //
+          // ),
+          // Container(
+          //   width: 100,
+          //   height: 100,
+          //   color: Colors.red,
+          //
+          // ),
+          // Container(
+          //   width: 100,
+          //   height: 100,
+          //   color: Colors.blue,
+          //
+          // ),
+          // Container(
+          //   width: 100,
+          //   height: 100,
+          //   color: Colors.red,
+          //
+          // ),
+          // Container(
+          //   width: 100,
+          //   height: 100,
+          //   color: Colors.blue,
+          //
+          // ),
+          // Container(
+          //   width: 100,
+          //   height: 100,
+          //   color: Colors.red,
+          //
+          // ),
+          // Container(
+          //   width: 100,
+          //   height: 100,
+          //   color: Colors.blue,
+          //
+          // ),
+        ],
+
+        ),
+/* ----------------------- End Column ------------------------------ */
     );
 
   }
