c456acd75afe87ba65e0fd69cb20c5e81bd9e891
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-03-06 21:21:48 +0600

    added: SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -17,17 +17,130 @@ class myApp extends StatelessWidget {
 
 }
 
+
+/* ------------------------ Without SingleChildScrollView ------------------------ */
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//           title: Text("Rajib Hasan"),
+//         ),
+//         body: Column(
+//           children: [
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.amber,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.blue,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.red,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.amber,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.blue,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.red,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.amber,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.blue,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.red,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.amber,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.blue,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.red,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.amber,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.blue,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.red,),
+//
+//           ],
+//         )
+//
+//     );
+//
+//   }
+// }
+
+
+
+
+/* ------------------------ SingleChildScrollView ------------------------ */
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         title: Text("Rajib Hasan"),
+//       ),
+//       body: SingleChildScrollView(
+//         child: Column(
+//           children: [
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.amber,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.blue,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.red,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.amber,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.blue,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.red,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.amber,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.blue,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.red,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.amber,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.blue,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.red,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.amber,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.blue,),
+//             Container(height: 100, width: MediaQuery.sizeOf(context).width, color: Colors.red,),
+//
+//           ],
+//         ),
+//       )
+//
+//     );
+//
+//   }
+// }
+
+
+/* ------------------------ SingleChildScrollView - Scroll direction - horizontal ------------------------ */
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Rajib Hasan"),
-      ),
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text("Scroll Direction"),
+        ),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              Container(height: 100, width: 100, color: Colors.amber,),
+              Container(height: 100, width: 100, color: Colors.blue,),
+              Container(height: 100, width: 100, color: Colors.red,),
+              Container(height: 100, width: 100, color: Colors.amber,),
+              Container(height: 100, width: 100, color: Colors.blue,),
+              Container(height: 100, width: 100, color: Colors.red,),
+              Container(height: 100, width: 100, color: Colors.amber,),
+              Container(height: 100, width: 100, color: Colors.blue,),
+              Container(height: 100, width: 100, color: Colors.red,),
+              Container(height: 100, width: 100, color: Colors.amber,),
+              Container(height: 100, width: 100, color: Colors.blue,),
+              Container(height: 100, width: 100, color: Colors.red,),
+              Container(height: 100, width: 100, color: Colors.amber,),
+              Container(height: 100, width: 100, color: Colors.blue,),
+              Container(height: 100, width: 100, color: Colors.red,),
+
+
+
+
+            ],
+          ),
+        )
+
     );
 
   }
 }
 
 
+
+/* ------------------------ SingleChildScrollView ------------------------ */
+
+
+
+
+
+/* ------------------------ SingleChildScrollView ------------------------ */
