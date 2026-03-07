9d7c6ac3005c9f9c24b317ce38981c636bb92927
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-03-08 04:14:22 +0600

    added: grid view builder

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/material.dart';
 import 'package:liveclass14/gridview-widget.dart';
 
+import 'gridview_builder_widget.dart';
+
 
 
 
@@ -13,7 +15,7 @@ class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: gridviewWidget(),
+        home: gridviewBuilderWidget(),
     );
   }
 
