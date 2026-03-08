3b1a23ebf0cf352abbde2721852205022cc088fb
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-03-09 05:44:11 +0600

    card sectioin added:

---
+++ lib/app_text_style.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyle{
+  static final titleBlack16 = GoogleFonts.playfairDisplay(
+    fontSize: 16,
+    color: Color(0xFF000000),
+    // color: Color(0xFFFFFFFF),
+    fontWeight: FontWeight.normal,
+  );
+  static final titleWhite16 = GoogleFonts.playfairDisplay(
+    fontSize: 16,
+    // color: Color(0xFF000000),
+    color: Color(0xFFFFFFFF),
+    fontWeight: FontWeight.normal,
+  );
+
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:liveclass14/app_text_style.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,26 +8,69 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 8),
+          child: Image.asset("assets/images/menu-outline.png"),
+        ),
+        leadingWidth: 50,
+        title: Text(
+          "Home",
+          style: AppTextStyle.titleBlack16,
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Color(0xFFCEC5D8).withOpacity(0.76),
+                image: DecorationImage(
+                  image: AssetImage("assets/images/bell.png"),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
       body: SafeArea(
         child: Column(
           children: [
-            // SizedBox(height: 30,),
-            Row(
-              children: [
-                Image.asset("assets/images/menu-outline.png"),
-                SizedBox(width: 10,),
-                Text("Home", style: GoogleFonts.playfairDisplay(
-                  fontSize: 26,
-                  color: Color(0xFF000000),
-                  fontWeight: FontWeight.normal,
-                ),)
-              ],
+            Container(
+              height: 150,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                image: DecorationImage(
+                  image: AssetImage("assets/images/bg.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: 30,),
+                  Text(
+                    "Today",
+                    style: AppTextStyle.titleWhite16,
+                  ),
+                  SizedBox(height: 10,),
+                  Text(
+                    "1st Ramadan 4445",
+                    style: AppTextStyle.titleWhite16,
+                  ),
+                  SizedBox(height: 10,),
+                  Text(
+                    "Tuesday, 12 Mar 2024",
+                    style: AppTextStyle.titleWhite16,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
-
       ),
-      
     );
   }
 }
