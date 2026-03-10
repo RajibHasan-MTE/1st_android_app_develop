b71bbb79f0c6d695fc9982e8384c97134726f10f
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-03-10 23:59:14 +0600

    added: basics code

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,72 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
+// Root Widget
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Rajib Hasan',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      home: CounterPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  // Show alert dialog
-  void showAlertDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text("Do you want to delete!"),
-        content: Text("Once Delete can't get back"),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text("Cancel"),
-          ),
-          TextButton(onPressed: () {}, child: Text("Ok")),
-        ],
-      ),
-    );
+// StatefulWidget
+class CounterPage extends StatefulWidget {
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+// State class (holds changing data)
+class _CounterPageState extends State<CounterPage> {
+  int count = 0; // state variable
+
+  void increment() {
+    setState(() {
+      count++;
+    });
   }
 
-  // Simple dialog for option choose
-  void showSimpleDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) => SimpleDialog(
-        title: Text("Do youwant to delete!"),
-        children: [
-          SimpleDialogOption(
-            onPressed: () => Navigator.pop(context),
-            child: Text("Dhaka"),
-          ),
-          SimpleDialogOption(
-            onPressed: () => Navigator.pop(context),
-            child: Text("London"),
-          ),
-        ],
-      ),
-    );
-  }
-
-  // Snack bar
-  void showSnackBar(BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text("Save Successfully"),
-        //
-      ),
-    );
-  }
-
-  // Bottom Sheet
-  void showBottomSheet(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      builder: (context) => Container(padding: EdgeInsets.all(20)),
-    );
+  void decrement() {
+    setState(() {
+      count--;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Dialog")),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              showAlertDialog(context);
-            },
-            child: Text("Alert Dialog"),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              showSimpleDialog(context);
-            },
-            child: Text("Simple dialog"),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              showSnackBar(context);
-            },
-            child: Text("show snack bar"),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              showBottomSheet(context);
-            },
-            child: Text("show bottom sheet"),
-          ),
-        ],
+      appBar: AppBar(
+        title: Text("StatefulWidget Demo"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Counter Value:",
+              style: TextStyle(fontSize: 22),
+            ),
+            Text(
+              "$count",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: increment,
+              child: Text("Increase"),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: decrement,
+              child: Text("Decrease"),
+            ),
+          ],
+        ),
       ),
     );
   }
