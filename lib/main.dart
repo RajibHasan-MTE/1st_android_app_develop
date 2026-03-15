20356b25cae30ab7182628c68126df410e53f523
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-03-16 00:43:13 +0600

    changed: home page

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       ),
       // home: BottomNavigationBarPage(),
       // home: TabBarPage(),
-      home: DrawerPage(),
+      home: TabBarPage(),
     );
   }
 }
