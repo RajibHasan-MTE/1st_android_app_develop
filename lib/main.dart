49a52b37ff785b2bef5c8bb7ac24811fc10e9807
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-03-30 12:07:38 +0600

    completed 1st toturial

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:liveclass14/todo_page.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Flutter'),
-        ),
-        body: Center(
-          child: Text('Welcome Rajib!'),
-        ),
-      ),
-    ),
-  );
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "todo app",
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: TodoPage(),
+    );
+  }
 }
---
+++ lib/todo_page.dart
@@ -0,0 +1,23 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TodoPage extends StatefulWidget {
+
+  @override
+  State<TodoPage> createState(){
+    return TodoPageView();
+  }
+}
+
+
+class TodoPageView extends State<TodoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Todo"),),
+      body: EdgeInsets.all(10),
+    );
+  }
+}
