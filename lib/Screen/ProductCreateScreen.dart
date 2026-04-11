d50a7c285ac1b084faaa1da98bbbaad81a142bc3
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-04-11 17:17:00 +0600

    competed: add style file

--- lib/Screen/ProductCreateScreen.dart
+++ lib/Screen/ProductCreateScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:liveclass14/Style/Style.dart';
 
 class ProductCreateScreen extends StatefulWidget {
   const ProductCreateScreen({super.key});
@@ -16,16 +17,18 @@ class _ProductCreateScreenState extends State<ProductCreateScreen> {
       ),
       body: Stack(
         children: [
+          // BackGround Graphics
           Container(
             child: (SingleChildScrollView(
               padding: EdgeInsets.all(10),
               child: Column(
                 children: [
-                  TextFormField(onChanged: (value){},),
-                  TextFormField(onChanged: (value){},),
-                  TextFormField(onChanged: (value){},),
-                  TextFormField(onChanged: (value){},),
-                  TextFormField(onChanged: (value){},),
+                  TextFormField(onChanged: (value){},decoration: AppInputDecoration('Product Name'),),
+                  TextFormField(onChanged: (value){},decoration: AppInputDecoration('Product Code'),),
+                  TextFormField(onChanged: (value){},decoration: AppInputDecoration('Product Image'),),
+                  TextFormField(onChanged: (value){},decoration: AppInputDecoration('Unit Price'),),
+                  TextFormField(onChanged: (value){},decoration: AppInputDecoration('Total Price'),),
+
                 ],
               ),
             )),
