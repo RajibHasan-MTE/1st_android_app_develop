0dc8470ad62d638892e2309237ad73c97fe9dc65
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-04-13 12:30:42 +0600

    added outlined button

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.crud.app"
+    namespace = "com.example.liveclass14"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.crud.app"
+        applicationId = "com.example.liveclass14"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- lib/Screen/ProductGridViewScreen.dart
+++ lib/Screen/ProductGridViewScreen.dart
@@ -1,9 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:liveclass14/Style/Style.dart';
 
 import '../RestAPI/RestClient.dart';
-
-
 
 class ProductGridViewScreen extends StatefulWidget {
   const ProductGridViewScreen({super.key});
@@ -15,51 +14,72 @@ class ProductGridViewScreen extends StatefulWidget {
 class _ProductGridViewScreenState extends State<ProductGridViewScreen> {
   List ProductList = [];
   bool Loading = true;
-  
+
   @override
-  void initState(){
+  void initState() {
     CallData();
     super.initState();
-    
   }
 
-  CallData() async{
+  CallData() async {
     Loading = true;
     var data = await ProductGridViewListRequest();
     setState(() {
       ProductList = data;
       Loading = false;
     });
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("List view"),),
+      appBar: AppBar(title: Text("List view")),
       body: Stack(
         children: [
           ScreenBackgrround(context),
           Container(
-            child: Loading ? (Center(child: CircularProgressIndicator(),)) : (
-            GridView.builder(
-                gridDelegate: ProductGridViewStyle(),
-                itemCount: ProductList.length,
-                itemBuilder: (context, index){
-                  return Card(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Expanded(child: Image.network(ProductList[index]['Img'], fit: BoxFit.fill,)),
-                        // Text("Product name"),
-                        // ElevatedButton(onPressed: (){}, child: Icon(Icons.delete),
-                      ],
-                    ),
-                  );
-                }
-            )
-            ),
-          )
+            child: Loading
+                ? (Center(child: CircularProgressIndicator()))
+                : (GridView.builder(
+                    gridDelegate: ProductGridViewStyle(),
+                    itemCount: ProductList.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Expanded(
+                              child: Image.network(
+                                ProductList[index]['Img'],
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.fromLTRB(5, 5, 5, 8),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(ProductList[index]['ProductName']),
+                                  SizedBox(height: 7,),
+                                  Text("Price: " + ProductList[index]['UnitPrice']+"BDT"),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      OutlinedButton(onPressed: (){}, child: Icon(CupertinoIcons.ellipsis_vertical_circle, size: 18, color: colorGreen,)),
+                                      SizedBox(width: 4,),
+                                      OutlinedButton(onPressed: (){}, child: Icon(CupertinoIcons.delete, size: 18, color: colorRed,)),
+                                    ],
+                                  ),
+
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  )),
+          ),
         ],
       ),
     );
